4408033e31b67b57448a8d2e9ad42e755a82b17c
GitLinuxDev <gitlinuxdev@gmail.com>
2019-08-29 02:10:24 -0300

    Upate - Elements & UL inside JavaScript

--- todos.js
+++ todos.js
@@ -1,1 +1,15 @@
-alert('App Todos');
+var listElement = document.querySelector('#app  ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+        'Fazer Cafe',
+        'Estudar Java Script',
+        'Comitar no Git Hub',
+        'Aprender Docker'
+];
+  
+
+
+
+]
