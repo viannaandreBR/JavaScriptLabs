2cbfa8d4006772df8b5c74cf7d51b0d34b300743
GitLinuxDev <gitlinuxdev@gmail.com>
2019-08-29 22:30:28 -0300

    Update - Get API with Axios in JavaScript

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-var minhaPromise = function () {
-  return new Promise(function(resolve, reject) {
-    var  xhr = new XMLHttpRequest();
-    xhr.open('Get','https://api.github.com/users/viannaandreBR');
-    xhr.send(null);
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else
-          reject('Erro na requisição');
-      }
-    }
-  } );
-}
-
-//var resultado = minhaPromise();
-//console.log(resultado);
-
-minhaPromise()
+axios.get('https://api.github.com/users/viannaandreBR')
   .then(function(response) {
     console.log(response);
+    document.write('nome ---->',response.data.name);
+    console.log(response.data.avatar_url);
   })
   .catch(function(error) {
     console.warn(error);
