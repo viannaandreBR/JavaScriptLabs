44c4113e0e599fba91d1e52065a57bce42349d6f
GitLinuxDev <gitlinuxdev@gmail.com>
2019-08-29 22:03:51 -0300

    Update - Async Promises in Java Script

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-var xhr = new XMLHttpRequest();
+var minhaPromise = function () {
+  return new Promise(function(resolve, reject) {
+    var  xhr = new XMLHttpRequest();
+    xhr.open('Get','https://api.github.com/users/viannaandreBR');
+    xhr.send(null);
 
-xhr.open('GET', 'https://api.github.com/users/viannaandreBR');
-xhr.send(null);
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else
+          reject('Erro na requisição');
+      }
+    }
+  } );
 }
+
+//var resultado = minhaPromise();
+//console.log(resultado);
+
+minhaPromise()
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  });
